78b099256d9e2b294248d1239161b94f80291bd7
雪路 翟 <1797310238@qq.com>
2026-03-13 10:43:10 +0800

    主窗体全屏

--- 工作日志/Main.Designer.cs
+++ 工作日志/Main.Designer.cs
@@ -48,6 +48,7 @@ namespace 对账平台
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.文件ToolStripMenuItem,
@@ -55,8 +56,8 @@ namespace 对账平台
             this.配置ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 32);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,41 +70,41 @@ namespace 对账平台
             this.下载账单ToolStripMenuItem,
             this.下载支付宝账单ToolStripMenuItem});
             this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
-            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(68, 22);
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(98, 28);
             this.文件ToolStripMenuItem.Text = "系统管理";
             // 
             // 添加医院信息ToolStripMenuItem
             // 
             this.添加医院信息ToolStripMenuItem.Name = "添加医院信息ToolStripMenuItem";
-            this.添加医院信息ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.添加医院信息ToolStripMenuItem.Size = new System.Drawing.Size(254, 34);
             this.添加医院信息ToolStripMenuItem.Text = "添加医院信息";
             this.添加医院信息ToolStripMenuItem.Click += new System.EventHandler(this.添加配置ToolStripMenuItem_Click);
             // 
             // 添加服务器信息ToolStripMenuItem
             // 
             this.添加服务器信息ToolStripMenuItem.Name = "添加服务器信息ToolStripMenuItem";
-            this.添加服务器信息ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.添加服务器信息ToolStripMenuItem.Size = new System.Drawing.Size(254, 34);
             this.添加服务器信息ToolStripMenuItem.Text = "添加服务器信息";
             this.添加服务器信息ToolStripMenuItem.Click += new System.EventHandler(this.添加服务器信息ToolStripMenuItem_Click);
             // 
             // 添加个人账号信息ToolStripMenuItem
             // 
             this.添加个人账号信息ToolStripMenuItem.Name = "添加个人账号信息ToolStripMenuItem";
-            this.添加个人账号信息ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.添加个人账号信息ToolStripMenuItem.Size = new System.Drawing.Size(254, 34);
             this.添加个人账号信息ToolStripMenuItem.Text = "添加个人账号信息";
             this.添加个人账号信息ToolStripMenuItem.Click += new System.EventHandler(this.添加个人账号信息ToolStripMenuItem_Click);
             // 
             // 下载账单ToolStripMenuItem
             // 
             this.下载账单ToolStripMenuItem.Name = "下载账单ToolStripMenuItem";
-            this.下载账单ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.下载账单ToolStripMenuItem.Size = new System.Drawing.Size(254, 34);
             this.下载账单ToolStripMenuItem.Text = "下载账单";
             this.下载账单ToolStripMenuItem.Click += new System.EventHandler(this.下载账单ToolStripMenuItem_Click);
             // 
             // 下载支付宝账单ToolStripMenuItem
             // 
             this.下载支付宝账单ToolStripMenuItem.Name = "下载支付宝账单ToolStripMenuItem";
-            this.下载支付宝账单ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.下载支付宝账单ToolStripMenuItem.Size = new System.Drawing.Size(254, 34);
             this.下载支付宝账单ToolStripMenuItem.Text = "下载支付宝账单";
             this.下载支付宝账单ToolStripMenuItem.Click += new System.EventHandler(this.下载支付宝账单ToolStripMenuItem_Click);
             // 
@@ -112,13 +113,13 @@ namespace 对账平台
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.对账日志ToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 22);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(62, 28);
             this.toolStripMenuItem1.Text = "功能";
             // 
             // 对账日志ToolStripMenuItem
             // 
             this.对账日志ToolStripMenuItem.Name = "对账日志ToolStripMenuItem";
-            this.对账日志ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.对账日志ToolStripMenuItem.Size = new System.Drawing.Size(182, 34);
             this.对账日志ToolStripMenuItem.Text = "对账日志";
             this.对账日志ToolStripMenuItem.Click += new System.EventHandler(this.对账日志ToolStripMenuItem_Click);
             // 
@@ -128,32 +129,34 @@ namespace 对账平台
             this.消息弹窗ToolStripMenuItem,
             this.自动下载配置ToolStripMenuItem});
             this.配置ToolStripMenuItem.Name = "配置ToolStripMenuItem";
-            this.配置ToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
+            this.配置ToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
             this.配置ToolStripMenuItem.Text = "配置";
             // 
             // 消息弹窗ToolStripMenuItem
             // 
             this.消息弹窗ToolStripMenuItem.Name = "消息弹窗ToolStripMenuItem";
-            this.消息弹窗ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.消息弹窗ToolStripMenuItem.Size = new System.Drawing.Size(218, 34);
             this.消息弹窗ToolStripMenuItem.Text = "消息弹窗";
             this.消息弹窗ToolStripMenuItem.Click += new System.EventHandler(this.消息弹窗ToolStripMenuItem_Click);
             // 
             // 自动下载配置ToolStripMenuItem
             // 
             this.自动下载配置ToolStripMenuItem.Name = "自动下载配置ToolStripMenuItem";
-            this.自动下载配置ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.自动下载配置ToolStripMenuItem.Size = new System.Drawing.Size(218, 34);
             this.自动下载配置ToolStripMenuItem.Text = "自动下载配置";
             this.自动下载配置ToolStripMenuItem.Click += new System.EventHandler(this.自动下载配置ToolStripMenuItem_Click);
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 675);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "对账平台（Account Reconciliation Platform）";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
